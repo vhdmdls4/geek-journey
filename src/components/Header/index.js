8264170e6bac7b7b4381777f08ba3822febddcfb
Victor <vhduartelima@hotmail.com>
2023-08-11 20:12:28 -0300

    feat: changing brand and adding some customization

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,14 @@
 import styles from './Header.module.scss';
 
-export default function Header({ titulo, descricao, className = '', imagem}) {
+export default function Header({ title, description, className = '' }) {
   return (
-    <header className={`${styles.header} ${className}`}>
-      <div className={styles['header-texto']}>
-        <h1>{titulo}</h1>
-        <h2>{descricao}</h2>
-      </div>
-      <div className={styles['header-imagem']}>
-        <img
-          alt={titulo}
-          src={imagem}
-        />
-      </div>
-    </header>
-  )
+    <div className={styles.background}>
+      <header className={`${styles.header} ${className}`}>
+        <div className={styles['header-texto']}>
+          <h1>{title}</h1>
+          <h2>{description}</h2>
+        </div>
+      </header>
+    </div>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,14 @@
 import Header from 'components/Header';
 import styles from './Home.module.scss';
-import relogio from 'assets/inicial.png';
 
 export default function Home() {
   return (
     <div>
       <Header
-        titulo='Classificados Tech'
-        descricao='Compre diversos tipos de produtos no melhor site do Brasil!'
-        imagem={relogio}
+        title="Geek Journey"
+        description="Transforme Sua Paixão em Coleções Reais"
         className={styles.header}
       />
     </div>
-  )
+  );
 }
