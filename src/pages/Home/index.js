91e4120d63c350b9ce5e05e82254a96f87c7af0f
Victor Hugo <victor.hugo@omnilogic.ai>
2023-09-02 18:41:11 -0300

    feat: adding categories

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,31 @@
 import Header from 'components/Header';
 import styles from './Home.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <Header className={styles.header} />
-      <main>TEstando aqui</main>
+      <main>
+        <div className={styles.categories}>
+          <div className={styles['categories-title']}>
+            <h1>Categorias</h1>
+          </div>
+          <div className={styles['categories-container']}>
+            {categories.map((category, index) => {
+              <div
+                key={index}
+                onClick={() => navigate(`/categoria/${category.id}`)}
+              >
+                <img src={category.thumbnail} alt={category.name} />
+                <h1>{category.name}</h1>
+              </div>;
+            })}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
