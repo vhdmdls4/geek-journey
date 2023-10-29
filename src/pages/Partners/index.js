b1493e531a2a02d029a7050d16670a5c72536eed
Victor Hugo <vhduartelima@hotmail.com>
2023-10-29 16:09:57 -0300

    feat: adding partners page

---
+++ src/pages/Partners/index.js
@@ -0,0 +1,11 @@
+import Header from 'components/Header';
+import React from 'react';
+
+export default function Partners() {
+  return (
+    <div>
+      <Header />
+      <div>Partners</div>
+    </div>
+  );
+}
