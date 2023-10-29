43861cfe9f1d70978c92f663f65607a401dc7958
Victor Hugo <vhduartelima@hotmail.com>
2023-10-29 16:07:09 -0300

    feat: adding partners and cart routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import PaginaPadrao from 'components/PaginaPadrao';
+import Cart from 'pages/Cart';
 import CategoryPage from 'pages/Category';
 import Home from 'pages/Home';
+import Partners from 'pages/Partners';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function Router() {
@@ -10,6 +12,8 @@ export default function Router() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Home />} />
           <Route path="/categoria/:categoryName" element={<CategoryPage />} />
+          <Route path="/parceiros-geek" element={<Partners />} />
+          <Route path="/carrinho" element={<Cart />} />
         </Route>
       </Routes>
     </BrowserRouter>
